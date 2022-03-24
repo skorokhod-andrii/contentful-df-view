fe8ed465d49df37979b7b2394507015936625a7c
Andrii <andriis@wix.com>
2022-03-24 14:22:10 +0200

    Removed not needed code. Added simple table that works. Start of tooltip

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,18 +1,121 @@
-import React from 'react';
-import { PlainClientAPI } from 'contentful-management';
-import { Paragraph } from '@contentful/f36-components';
-import { FieldExtensionSDK } from '@contentful/app-sdk';
-
+import React, { useEffect, useState } from "react";
+import { PlainClientAPI } from "contentful-management";
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  EditorToolbarButton,
+  Button,
+} from "@contentful/forma-36-react-components";
+import { FieldExtensionSDK } from "@contentful/app-sdk";
+import { v4 as uuid } from "uuid";
+import { TextInput } from "@contentful/f36-components";
 interface FieldProps {
   sdk: FieldExtensionSDK;
   cma: PlainClientAPI;
 }
 
+interface TrainingPhraseItem {
+  id: string;
+  text: string;
+  entityLables: EntityLabel[];
+}
+
+interface EntityLabel {
+  entity: string;
+  startPos: number;
+  endPos: number;
+}
+
+function createTrainingPhrase(): TrainingPhraseItem {
+  return {
+    id: uuid(),
+    text: "",
+    entityLables: [],
+  };
+}
 const Field = (props: FieldProps) => {
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+
+  const [items, setItems] = useState<TrainingPhraseItem[]>([]);
+
+  useEffect(() => {
+    // This ensures our app has enough space to render
+    props.sdk.window.startAutoResizer();
+
+    // Every time we change the value on the field, we update internal state
+    props.sdk.field.onValueChanged((value: TrainingPhraseItem[]) => {
+      if (Array.isArray(value)) {
+        setItems(value);
+      }
+    });
+  });
+
+  /** Adds another item to the list */
+  const addNewItem = () => {
+    props.sdk.field.setValue([...items, createTrainingPhrase()]);
+  };
+
+  const onTrainingPhraseChange =
+    (item: TrainingPhraseItem) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const selectedItem = items.find((i) => i.id === item.id);
+      if (selectedItem) {
+        selectedItem.text = event.target.value;
+      }
+      props.sdk.field.setValue(items);
+    };
+
+  /** Deletes an item from the list */
+  const deleteItem = (item: TrainingPhraseItem) => {
+    props.sdk.field.setValue(items.filter((i) => i.id !== item.id));
+  };
+  const openTooltip = (item: TrainingPhraseItem) => (event: React.MouseEvent<HTMLInputElement>) =>{
+    const selection = window.getSelection();
+    if(selection){
+      //TODO open tooltip
+    }
+  }
+  return (
+    <div>
+      <Table>
+        <TableBody>
+          {items.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>
+                <TextInput
+                  id="text"
+                  name="text"
+                  width="full"
+                  value={item.text}
+                  onChange={onTrainingPhraseChange(item)}
+                  onMouseUpCapture={openTooltip(item)}
+                />
+              </TableCell>
+              <TableCell align="right">
+                <EditorToolbarButton
+                  label="delete"
+                  icon="Delete"
+                  onClick={() => deleteItem(item)}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Button
+        buttonType="naked"
+        onClick={addNewItem}
+        icon="PlusCircle"
+        style={{ marginTop: "5px" }}
+      >
+        Add user expression
+      </Button>
+    </div>
+  );
 };
 
 export default Field;
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createClient } from 'contentful-management';
+import '@contentful/forma-36-react-components/dist/styles.css';
 
 import {
-  AppExtensionSDK,
+  // AppExtensionSDK,
   FieldExtensionSDK,
-  SidebarExtensionSDK,
-  DialogExtensionSDK,
-  EditorExtensionSDK,
-  PageExtensionSDK,
+  // SidebarExtensionSDK,
+  // DialogExtensionSDK,
+  // EditorExtensionSDK,
+  // PageExtensionSDK,
   init,
   locations,
 } from '@contentful/app-sdk';
 import type { KnownSDK } from '@contentful/app-sdk';
 import { GlobalStyles } from '@contentful/f36-components';
 
-import ConfigScreen from './components/ConfigScreen';
-import EntryEditor from './components/EntryEditor';
-import Page from './components/Page';
-import Sidebar from './components/Sidebar';
+// import ConfigScreen from './components/ConfigScreen';
+// import EntryEditor from './components/EntryEditor';
+// import Page from './components/Page';
+// import Sidebar from './components/Sidebar';
 import Field from './components/Field';
-import Dialog from './components/Dialog';
+// import Dialog from './components/Dialog';
 import LocalhostWarning from './components/LocalhostWarning';
 
 if (process.env.NODE_ENV === 'development' && window.self === window.top) {
@@ -50,30 +51,30 @@ if (process.env.NODE_ENV === 'development' && window.self === window.top) {
     // Feel free to remove unused locations
     // Dont forget to delete the file too :)
     const ComponentLocationSettings = [
-      {
-        location: locations.LOCATION_APP_CONFIG,
-        component: <ConfigScreen cma={cma} sdk={sdk as AppExtensionSDK} />,
-      },
+      // {
+      //   location: locations.LOCATION_APP_CONFIG,
+      //   component: <ConfigScreen cma={cma} sdk={sdk as AppExtensionSDK} />,
+      // },
       {
         location: locations.LOCATION_ENTRY_FIELD,
         component: <Field cma={cma} sdk={sdk as FieldExtensionSDK} />,
       },
-      {
-        location: locations.LOCATION_ENTRY_EDITOR,
-        component: <EntryEditor cma={cma} sdk={sdk as EditorExtensionSDK} />,
-      },
-      {
-        location: locations.LOCATION_DIALOG,
-        component: <Dialog cma={cma} sdk={sdk as DialogExtensionSDK} />,
-      },
-      {
-        location: locations.LOCATION_ENTRY_SIDEBAR,
-        component: <Sidebar cma={cma} sdk={sdk as SidebarExtensionSDK} />,
-      },
-      {
-        location: locations.LOCATION_PAGE,
-        component: <Page cma={cma} sdk={sdk as PageExtensionSDK} />,
-      },
+      // {
+      //   location: locations.LOCATION_ENTRY_EDITOR,
+      //   component: <EntryEditor cma={cma} sdk={sdk as EditorExtensionSDK} />,
+      // },
+      // {
+      //   location: locations.LOCATION_DIALOG,
+      //   component: <Dialog cma={cma} sdk={sdk as DialogExtensionSDK} />,
+      // },
+      // {
+      //   location: locations.LOCATION_ENTRY_SIDEBAR,
+      //   component: <Sidebar cma={cma} sdk={sdk as SidebarExtensionSDK} />,
+      // },
+      // {
+      //   location: locations.LOCATION_PAGE,
+      //   component: <Page cma={cma} sdk={sdk as PageExtensionSDK} />,
+      // },
     ];
 
     // Select a component depending on a location in which the app is rendered.
