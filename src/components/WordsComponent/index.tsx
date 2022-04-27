ee6c3993806039e34b632b10d649c7c007085d30
Andrii <andriis@wix.com>
2022-04-27 17:37:22 +0300

    remove not needed code

--- src/components/WordsComponent/index.tsx
+++ src/components/WordsComponent/index.tsx
@@ -18,33 +18,6 @@ export const WordsComponent = (props: WordsComponentProps) => {
         word={word}
       />
     );
-    // return (
-    //   <span
-    //     id={`${phrase.id}-${index}`}
-    //     data-count={index}
-    //     key={index}
-    //     style={{
-    //       backgroundColor: word.isHighlighted ? gerRandomColor() : "white",
-    //     }}
-    //   >
-    //     {index === 0 ? word.text : ` ${word.text}`}
-    //   </span>
-    // );
   });
   return <>{formattedHtml}</>;
-};
-
-const gerRandomColor = () => {
-  const colors = [
-    "rgb(215, 204, 200)",
-    "rgb(175, 255, 200)",
-    "rgb(159, 236, 254)",
-    "rgb(210, 195, 234)",
-    "rgb(255, 205, 246)",
-    "rgb(255, 209, 175)",
-    "rgb(218, 223, 227)",
-    "rgb(255, 253, 166)",
-    "rgb(255, 209, 175)",
-  ];
-  return colors[Math.floor(Math.random() * colors.length)];
 };
